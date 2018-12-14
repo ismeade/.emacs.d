1fa9b5e8a7a2aa73f89e5e4d1e48ec8df22fbee0
liyang <ismeade99@sina.com>
2018-12-14 16:55:40 +0800

    up

---
+++ lisp/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ssh counsel ivy swiper smex web-mode popwin js2-mode evil darcula-theme company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
